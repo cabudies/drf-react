aee12b780020776247da21ec58f5d0e16774c11f
Gurjas Singh <gurjassingh92@gmail.com>
2022-09-10 00:28:34 +0530

    react+drf axios call with useState and useEffect

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
+import axios from 'axios'
 
 import Rating from '../components/Rating'
 import products from '../products'
@@ -8,7 +9,17 @@ import products from '../products'
 
 function ProductScreen() {
     const { id } = useParams();
-    const product = products.find((p) => p._id === id)
+    // const product = products.find((p) => p._id === id)
+    const [product, setProduct] = useState([]);
+
+    useEffect(()=> {
+        async function fetchParticularProductFromAPI() {
+            const { data } = await axios.get(`/api/products/${id}`)
+            setProduct(data)
+        }
+
+        fetchParticularProductFromAPI()
+    }, [])
 
     return (
         <div>
