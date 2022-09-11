e278272f298e6a7172e3b7edcfb94eede8cdc392
Gurjas Singh <gurjassingh92@gmail.com>
2022-09-11 16:43:32 +0530

    working on serializer and db models and image upload

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -5,7 +5,9 @@ from . import views
 urlpatterns = [
     path('', view=views.getRoutes, name='routes'),
     path('products', view=views.getProducts, name='products'),
+    path('products-db', view=views.getProductsListFromDB, name='products-db'),
     path('products/<str:pk>', view=views.getParticularProduct, name='particular-product'),
+    path('products-db/<str:pk>', view=views.getProductsByIDFromDB, name='particular-product-db'),
 ]
 
 
--- backend/base/views.py
+++ backend/base/views.py
@@ -4,6 +4,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .products import products as products_list
+from .models import Product as ProductDBModel
+from .serializers import ProductSerializer
 
 # Create your views here.
 
@@ -17,6 +19,20 @@ def getRoutes(request):
 @api_view(["GET"])
 def getProducts(request):
     return Response(products_list)
+
+
+@api_view(["GET"])
+def getProductsByIDFromDB(request, pk):
+    products = ProductDBModel.objects.get(_id=pk)
+    serialized_data = ProductSerializer(products, many=False)
+    return Response(serialized_data.data)
+
+
+@api_view(["GET"])
+def getProductsListFromDB(request):
+    products_list = ProductDBModel.objects.all()
+    serialized_data = ProductSerializer(products_list, many=True)
+    return Response(serialized_data.data)
 
 
 @api_view(["GET"])
